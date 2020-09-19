e49fce5afd5a63fff779c08a0f1294ed1e5378e2
Stephen William <stevedelta99@yahoo.com>
2020-09-19 13:19:44 +0700

    Change naming for search api

--- src/components/api-call/api.js
+++ src/components/api-call/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const CORS_EVERYWHERE = 'https://cors-anywhere.herokuapp.com'
 const CITY_URL = 'https://developers.zomato.com/api/v2.1/cities'
-const RESTAURANTS_URL = 'https://developers.zomato.com/api/v2.1/search'
+const SEARCH_URL = 'https://developers.zomato.com/api/v2.1/search'
 const API_KEY = process.env.REACT_APP_ZOMATO_API_KEY
 
 export const fetchCityData = async (city) => {
@@ -20,13 +20,13 @@ export const fetchCityData = async (city) => {
 }
 
 export const fetchRestaurants = async(city_id, query) => {
-    const response = await axios.get(`${CORS_EVERYWHERE}/${RESTAURANTS_URL}`, {
+    const response = await axios.get(`${CORS_EVERYWHERE}/${SEARCH_URL}`, {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'user-key': `${API_KEY}`
         },
-        params: {
+        params: {   
             entity_id: `${city_id}`,
             entity_type: 'city',
             q: query,
